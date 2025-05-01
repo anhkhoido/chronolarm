6f9ad8cca1bcf62068c22ead9034f03f4a2b7bb4
anhkhoido <anhkhoido@gmail.com>
2025-05-01 11:54:03 -0400

    chr002 - Refactoring jQuery.

--- script/script.js
+++ script/script.js
@@ -10,6 +10,27 @@ $("#pauseButton").on("click", function() {
     alert('pause!');
 });
 
+$(function () {
+    function updateLength(text) {
+        alert(text);
+    }
+    const actions = {
+        incrementLengthWorkout: () => updateLength("Increase length workout"),
+        decrementLengthWorkout: () => updateLength('Decrease length workout'),
+        incrementLengthSet: () => updateLength('Increase length set'),
+        decrementLengthSet: () => updateLength('Decrease length set'),
+        incrementLengthBreak: () => updateLength('Increase length break'),
+        decrementLengthBreak: () => updateLength('Decrease length break')
+    };
+
+    $("button").on("click", function () {
+        const id = this.id;
+        if (actions[id]) {
+            actions[id]();
+        }
+    });
+});
+
 $(function() {
     $("div#chronolarm").find("input").attr("readonly", true).val("00 : 00");
 });
